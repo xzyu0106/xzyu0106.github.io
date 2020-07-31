0caa6e48308a28e5146d850ba4a57055a2482333
Xuezhi Yu <xzyu0106@foxmail.com>
2020-07-31 23:51:51 +0800

    commit myblog

---
+++ post/3一键发布hugo博客.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cd ..
+cd ..
+
+hugo --theme=maupassant --baseUrl="https://simplifythewhale.com" --buildDrafts
+
+sleep 5
+
+cd public
+
+git config --global core.autocrlf false
+
+sleep 2
+
+git add .
+
+sleep 5
+
+git commit -m "commit myblog"
+
+sleep 5
+
+git remote add origin git@github.com:xzyu0106/xzyu0106.github.io.git
+
+sleep 5
+
+git push -u origin master
+
+sleep 20
+
+scp -r D:/hugo/sites/myblog/public/* root@121.41.229.169:/home/hugo

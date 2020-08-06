8ccbed057ee4167c9043ccab68e8ecf9f80dcd54
Xuezhi Yu <xzyu0106@foxmail.com>
2020-08-06 13:15:59 +0800

    commit myblog

--- post/双击一键发布博客.bat
+++ post/双击一键发布博客.bat
@@ -1,21 +1,21 @@
 d:
 cd \hugo\sites\myblog\
 xcopy /s /i /y D:\hugo\sites\myblog\content\post_images D:\hugo\sites\myblog\content\post\post_images
-TIMEOUT /T 3
+TIMEOUT /T 1
 hugo --theme=maupassant --baseUrl="https://simplifythewhale.com" --buildDrafts
-TIMEOUT /T 3
+TIMEOUT /T 1
 rd /s /q D:\hugo\sites\myblog\content\post\post_images
 cd public
-TIMEOUT /T 5
+TIMEOUT /T 1
 git config --global core.autocrlf false
-TIMEOUT /T 3
+TIMEOUT /T 1
 git add .
-TIMEOUT /T 3
+TIMEOUT /T 1
 git commit -m "commit myblog"
-TIMEOUT /T 3
+TIMEOUT /T 1
 git remote add origin git@github.com:xzyu0106/xzyu0106.github.io.git
-TIMEOUT /T 4
+TIMEOUT /T 1
 git push -u origin master
-TIMEOUT /T 10
+TIMEOUT /T 20
 scp -r D:/hugo/sites/myblog/public/* root@121.41.229.169:/home/hugo
 TIMEOUT /T 3

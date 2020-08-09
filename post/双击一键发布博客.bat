293dc39668be7768f5b9fc4f6a41209fcb5eb7e9
Xuezhi Yu <xzyu0106@foxmail.com>
2020-08-09 13:51:46 +0800

    commit myblog

--- post/双击一键发布博客.bat
+++ post/双击一键发布博客.bat
@@ -2,7 +2,7 @@ d:
 cd \hugo\sites\myblog\
 xcopy /s /i /y D:\hugo\sites\myblog\content\post_images D:\hugo\sites\myblog\content\post\post_images
 TIMEOUT /T 1
-hugo --theme=maupassant --baseUrl="https://simplifythewhale.com" --buildDrafts
+hugo --theme=maupassant --baseUrl="https://simplifythewhale.com"
 TIMEOUT /T 1
 rd /s /q D:\hugo\sites\myblog\content\post\post_images
 cd public
